b867d72c50befb2afba2b7975646c45b4e22dfce
Petro Hubenya <124573077+PetroHubenya@users.noreply.github.com>
2023-03-31 10:31:07 +0300

    First commit. Create WEB API project

---
+++ Angular_.Net_SQL_Server_Project/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Angular_.Net_SQL_Server_Project
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
