2bd2a007de394474dc6ee092b187ca285d8e20a0
Petro Hubenya <p.hubenya@gmail.com>
2023-03-31 11:33:06 +0300

    Create Models

---
+++ Angular_.Net_SQL_Server_Project/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace Angular_.Net_SQL_Server_Project.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
